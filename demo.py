f7276c450e2aaf5f4823bbbf0c1d1ce3f20d1468
GautamSavsaviya <gautamsavasvaiya222@gmail.com>
2022-06-27 18:49:34 +0530

    initial commit

---
+++ demo.py
@@ -0,0 +1,36 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "This is home page"
+
+
+@app.route("/admin")
+def admin():
+    return "admin page"
+
+
+@app.route("/student")
+def student():
+    return "Student page"
+
+
+@app.route("/management")
+def management():
+    return "Management page"
+
+
+@app.route("/user/<name>")
+def user(name):
+    if name == 'admin':
+        return redirect(url_for("admin"))
+    elif name == 'student':
+        return redirect(url_for("student"))
+    elif name == 'management':
+        return redirect(url_for("management"))
+
+
+app.run(debug=True)
